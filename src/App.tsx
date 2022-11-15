8c3d4d979b61d8e6c77926793539ec92e9fde710
siykt <cnsiykt@163.com>
2022-11-15 21:48:39 +0800

    feat: TabAssetsAndGalleryPlugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import { useEffect, useRef, useState } from 'react';
 import { Viewer } from 'photo-sphere-viewer';
+import { GalleryPlugin } from 'photo-sphere-viewer/dist/plugins/gallery';
 import './App.css';
 import 'photo-sphere-viewer/dist/photo-sphere-viewer.css';
+import 'photo-sphere-viewer/dist/plugins/gallery.css';
+import { TAB_DATA_MAPS, ZH_CN_LANG } from './constants';
 
 function App() {
   const appRef = useRef<HTMLDivElement>(null);
+  const viewerRef = useRef<Viewer | null>(null);
+  const [tab, setTab] = useState(new URL(window.location.href).searchParams.get('tab') ?? 'lssy');
 
   useEffect(() => {
-    if (appRef.current) {
-      const viewer = new Viewer({
+    if (!appRef.current) return;
+    const tabData = TAB_DATA_MAPS[tab];
+    if (!tabData) return;
+
+    let viewer = viewerRef.current;
+    if (!viewer) {
+      viewer = new Viewer({
         container: appRef.current,
         touchmoveTwoFingers: true,
+        plugins: [[GalleryPlugin, { visibleOnLoad: true }]],
         mousewheelCtrlKey: true,
-        panorama: 'http://filebed.xxlsjfx.com/win/2022-11-14-1668405547658.jpg',
+        panorama: tabData.assets[0],
+        lang: ZH_CN_LANG,
       });
+      viewerRef.current = viewer;
     }
+    viewer.getPlugin(GalleryPlugin)?.setItems(
+      tabData.assets.map((panorama, index) => ({
+        id: `${tab}-${index}`,
+        panorama,
+        name: `${tabData.name}-${index + 1}`,
+      }))
+    );
   });
 
   return <div ref={appRef} className='App'></div>;

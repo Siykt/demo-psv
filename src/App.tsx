f824440596f133f46e822719223d944c9a8003bb
siykt <cnsiykt@163.com>
2022-11-15 22:12:56 +0800

    feat: update page title

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import './App.css';
 import 'photo-sphere-viewer/dist/photo-sphere-viewer.css';
 import 'photo-sphere-viewer/dist/plugins/gallery.css';
 import { TAB_DATA_MAPS, ZH_CN_LANG } from './constants';
+import { useTitle } from 'react-use';
 
 function App() {
   const appRef = useRef<HTMLDivElement>(null);
   const viewerRef = useRef<Viewer | null>(null);
   const [VRId, setVRId] = useState(new URL(window.location.href).searchParams.get('VRId') ?? 'lssy');
+  const [title, setTitle] = useState('Loading...');
+
+  useTitle(title);
 
   useEffect(() => {
     if (!appRef.current) return;
@@ -21,20 +25,23 @@ function App() {
       viewer = new Viewer({
         container: appRef.current,
         touchmoveTwoFingers: true,
-        plugins: [[GalleryPlugin, { visibleOnLoad: true }]],
+        plugins: [[GalleryPlugin, {}]],
         mousewheelCtrlKey: true,
         panorama: tabData.assets[0],
         lang: ZH_CN_LANG,
       });
       viewerRef.current = viewer;
     }
-    viewer.getPlugin(GalleryPlugin)?.setItems(
+    const gp = viewer.getPlugin(GalleryPlugin);
+    if (!gp) return;
+    gp.setItems(
       tabData.assets.map((panorama, index) => ({
         id: `${VRId}-${index}`,
         panorama,
         name: `${tabData.name}-${index + 1}`,
       }))
     );
+    setTitle(tabData.name);
   }, [VRId]);
 
   return <div ref={appRef} className='App'></div>;
